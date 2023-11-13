517ea64791d0800258c76633e9def5336d1f8f60
mfhaisall <mhfaisal011@gmail.com>
2023-11-13 23:37:32 +0700

    perubahan 1 error bagian menus

--- iceneko/models.py
+++ iceneko/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class beranda(models.Model):
+    nama = models.CharField(max_length=100)
+    keterangan = models.TextField()
+
+    def __str__(self):
+        return f"{self.nama}"
+    
+class menu(models.Model):
+    nama = models.CharField(max_length=100)
+   
+
+    def __str__(self):
+        return f"{self.nama}"
+    
+
+    
